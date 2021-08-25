0e9f2993eed061e7b64a257fb692b21575f70321
kamath7 <adithyakamath96@gmail.com>
2021-08-26 01:43:35 +0530

    allowing certain methods only

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(cors({
-    origin: "*"
+    origin: "*",
+    methods: ["GET","POST"] //only allows get and post methods
 }))
 app.get('/data',(req,res)=>{
     res.send({food:'Prawns'})
